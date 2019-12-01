c2f87f4a9d98336559207ff68687dfd8ed5bfbdc
Drew Ellingson <drew.ellingson@gmail.com>
2019-11-30 22:54:46 -0800

    cleaned up

--- day_1/p1.py
+++ day_1/p1.py
@@ -1,9 +1,6 @@
 with open('input.txt') as input:
     lines = [int(line.strip()) for line in input.readlines()]
 
-for line in lines:
-    print(line)
-
 fuel = [int(line / 3) - 2 for line in lines]
 
 print(sum(fuel))
